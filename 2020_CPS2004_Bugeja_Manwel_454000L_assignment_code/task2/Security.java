f42bffd329640f0a96582e61779c83d060665d64
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2019-11-20 10:39:13 +0100

    implemented method to show all instances

--- 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/task2/Security.java
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/task2/Security.java
@@ -1,27 +1,32 @@
 import java.util.ArrayList;
 
-class Security {
-    private static ArrayList instances = new ArrayList();
+class Security{
+    private static ArrayList<Security> instances = new ArrayList<Security>();
 
     private String description;
     private double price;
     private int supply;
 
-    public static void createSecurity(String description, double price, int supply) {
+    public static void createSecurity(String description, double price, int supply){
         Security instance = new Security();
 
         instance.description = description;
         instance.price = price;
         instance.supply = supply;
 
-        instances.add(new java.lang.ref.WeakReference(instance));
+        instances.add(instance);
     }
 
     public static void showInstances(){
+        for(int i = 0; i < instances.size(); ++i){
+            System.out.println("Description: " + instances.get(i).description);
+            System.out.println("Price: " + instances.get(i).price);
+            System.out.println("Supply: " + instances.get(i).supply);
+            System.out.println();
+        }
     }
 
-    public static ArrayList getInstances() {
+    public static ArrayList getInstances(){
         return instances;
     }
 }
-
