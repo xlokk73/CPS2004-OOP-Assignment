4ddcbee1b6d6266c39aa36e98538ddeb51ac6b09
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2019-11-23 15:17:17 +0100

    added menu

--- 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/task2/Main.java
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/task2/Main.java
@@ -21,11 +21,50 @@ class Main{
             System.out.println("1. exchange");
             System.out.println("2. register");
             System.out.println("3. list");
-            System.out.println("4. trade");
+            System.out.println("4. book");
             System.out.println("5. cancel order");
             System.out.println();
 
             input = sc.nextInt();
+
+
+            switch(input){
+                case 1:
+                    fnExchange();
+                    break;
+                case 2:
+                    fnRegister();
+                    break;
+                case 3:
+                    fnList();
+                    break;
+                case 4:
+                    fnBook();
+                    break;
+                case 5:
+                    fnCancel();
+                    break;
+                default:
+                    System.out.println("Error: not an option");
+                    break;
+            }
         }
     }
+
+    public static void fnExchange(){
+        System.out.println("exchanging");
+    }
+    public static void fnRegister(){
+        System.out.println("registering");
+    }
+    public static void fnList(){
+        System.out.println("listing");
+    }
+    public static void fnBook(){
+        System.out.println("booking");
+    }
+    public static void fnCancel(){
+        System.out.println("canceling order");
+    }
+
 }
