1066cd21d83c3559099f41262de0c42a123772f8
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2019-11-24 09:52:04 +0100

    same users cannot be added anymore

--- 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/task2/User.java
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/task2/User.java
@@ -10,7 +10,7 @@ class User{
         User instance = new User();
 
         for(int i = 0; i < instances.size(); ++i){
-            if(instances.get(i).userName == name){
+            if(instances.get(i).userName.equals(name)){
                 System.out.println("Error: User already exists");
                 return;
             }
