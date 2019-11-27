f32a108a36dcc17b8c011cbd941c68f68aa7b732
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2019-11-27 20:17:22 +0100

    restarted project structure from scratch

--- 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/task1/immutable_list.hpp
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/task1/immutable_list.hpp
@@ -1,5 +1,69 @@
-#include <list>
+#include <iostream>
+
+template<typename T>
+class immutable_list{
+    private:
+        bool is_empty;
+        T value;
+        immutable_list<T>* next_ptr;
+
+        immutable_list<T>(T, bool);
+
+    public:
+        immutable_list<T>(void);
+        immutable_list<T>* copy(void);
+        immutable_list<T>* add(T);
+        immutable_list<T>* remove(void);
+        immutable_list<T>* clear(void);
+
+        void print(void);
+};
+
+template<typename T>
+immutable_list<T>::immutable_list(void){
+    this->is_empty = true;
+    this->value = 0;
+    this->next_ptr = nullptr;
+}
+
+template<typename T>
+immutable_list<T>::immutable_list(T new_value, bool is_empty){
+    this->is_empty = is_empty;
+    this->value = new_value;
+    this->next_ptr = nullptr;
+}
+
+template<typename T>
+void immutable_list<T>::print(void){
+    if(this->is_empty){
+        std::cout << std::endl;
+        return;
+    }
+
+    std::cout << this->value << " ";
+    this->next_ptr->print();
+}
 
 template<class T>
-using immutable_list = const std::list<T>;
+immutable_list<T>* immutable_list<T>::copy(void){
+    immutable_list<T>* new_list = new immutable_list<T>();
     
+    if(this->is_empty){
+        return new_list;
+    }
+
+    else{
+        new_list = new immutable_list<T>(this->value, false);
+        new_list->next_ptr = this->next_ptr->copy();
+    }
+    
+    return new_list;
+}
+
+template<typename T>
+immutable_list<T>* immutable_list<T>::add(T new_value){
+    immutable_list<T>* new_list = this->copy();
+    immutable_list<T>* new_node = new immutable_list(new_value, false);
+    new_node->next_ptr = new_list;
+    return new_node;
+}
--- 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/task1/main.cpp
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/task1/main.cpp
@@ -3,6 +3,14 @@
 
 int main(void){
     std::cout << "Hello, World!" << std::endl;
-    immutable_list<int> test = {1, 2, 3};
+
+    immutable_list<int>* list1 = new immutable_list<int>(); 
+    immutable_list<int>* list2 = list1->add(2);
+    immutable_list<int>* list3 = list2->add(3);
+    std::cout << "printing list3" << std::endl;
+    list3->print();
+    std::cout << "printing list2" << std::endl;
+    list2->print();
+
     return 0;
 }
