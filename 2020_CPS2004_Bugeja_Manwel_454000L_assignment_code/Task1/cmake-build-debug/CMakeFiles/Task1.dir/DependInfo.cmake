3e45a9d38e4f0d606a4f01e46d6b1bf88be79e6e
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2019-12-20 19:40:08 +0100

    Updated

---
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task1/cmake-build-debug/CMakeFiles/Task1.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/manwelbugeja/School/oop-assignment/2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task1/main.cpp" "/Users/manwelbugeja/School/oop-assignment/2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task1/cmake-build-debug/CMakeFiles/Task1.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task1/cmake-build-debug/CMakeFiles/Task1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task1.dir/main.cpp.o"
+  "Task1"
+  "Task1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
