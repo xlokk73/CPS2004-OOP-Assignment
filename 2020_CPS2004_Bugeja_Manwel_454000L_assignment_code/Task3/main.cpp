c9c7136c0401dd9cf6dcd62f52cb42f9d663bc9c
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2019-12-22 13:35:56 +0100

    Updated Plus class

--- 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task3/main.cpp
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task3/main.cpp
@@ -10,24 +10,48 @@ public:
     double eval() {
         return BinOp::apply(l_, r_);
     }
+
+
+};
+
+class Var {
+    double& v;
+
+public:
+    Var(double& val) : v(val){}
+    double eval() {
+        return v;
+    }
+};
+
+class Const {
+    const double v;
+
+public:
+    Const(double v) : v(v) {}
+    double eval() {
+        return v;
+    }
 };
 
 class Plus {
-    inline double apply(double a, double b) {
+    template <typename L, typename R>
+    inline double apply(L a, R b) {
         return (a + b);
     }
 };
 
-template <typename L, typename R>
+template <typename L, typename Plus, typename R>
 Expr<L, Plus, R> operator+(const L& l, const R& r) {
     return Expr<L, Plus, R>(l, r);
 };
 
-
 int main() {
     std::cout << "Hello, World!" << std::endl;
-
-//    double res = ((x() * x())) + c(14)).eval(4);
+//    Var x = new Var();
+//    Const c(14);
+//
+//    double res = ((x + x).eval(4);
 
     return 0;
 }
