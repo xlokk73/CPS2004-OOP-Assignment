9e6aa44effebf69318b334bd9fb05f6f135352d4
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2019-12-24 16:50:00 +0100

    Added concurrency testing on ImmutableLinkedList

--- 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task1/main.cpp
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task1/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <thread>
+#include <string>
 #include "ImmutableLinkedList.h"
+
+void func(ImmutableLinkedList<int>* list, int n) {
+    std::cout << "adding " << n << " from within new thread" << std::endl;
+    auto newList = list->push(n);
+    std::cout << "printing edited list" << std::endl;
+    newList->print();
+    std::cout << std::endl;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
@@ -9,16 +19,38 @@ int main() {
     auto list3 = list2->push(3);
     std::cout << "printing list3" << std::endl;
     list3->print();
+    std::cout << std::endl;
+
     std::cout << "printing list2" << std::endl;
     list2->print();
+    std::cout << std::endl;
 
     auto list4 = list3->push(4)->push(5)->push(6);
     std::cout << "printing list4" << std::endl;
     list4->print();
+    std::cout << std::endl;
 
     auto list5 = list4->pop();
     std::cout << "printing list5" << std::endl;
     list5->print();
+    std::cout << std::endl;
+
+    auto list6 = new ImmutableLinkedList<int>();
+    auto list7 = list6->push(10)->push(9)->push(8)->push(7);
+    std::cout << "printing list6" << std::endl;
+    list7->print();
+    std::cout << std::endl;
+
+    std::cout << "new thread with list7 passed" << std::endl;
+    std::thread th1(&func, list7, 6);
+    std::cout << "new thread with list7 passed" << std::endl;
+    std::thread th2(&func, list7, 5);
+    th1.join();
+    th2.join();
+
+    std::cout << "Outside thread" << std::endl;
+    std::cout << "printing list7"<< std::endl;
+    list7->print();
 
     return 0;
 }
