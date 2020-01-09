ccf8fb1f2e68d64a5d57fe750cb5fad29b4adb81
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2020-01-10 00:18:29 +0100

    added exchangePlatformTest and found bug

---
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task2/test/ExchangePlatformTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExchangePlatformTest {
+
+    @Test
+    public void exchangePlatformTest1(){
+        Lister.register("Manwel", 10000);
+        Security.list("Manwel", "building", 100, 5);
+
+        try{
+            assertTrue(Trader.owns("Manwel", "building", 5));
+        }
+        catch(NonExistingException e){
+            fail();
+        }
+
+        Trader.register("Christian", 10000);
+        BuyOrder.book("Christian", "building", 100, 5);
+
+        assertTrue(BuyOrder.exists("building", 100, 5));
+
+        try{
+            assertFalse(Trader.owns("Christian", "building", 5));
+        }
+        catch(NonExistingException e){
+            fail();
+        }
+
+        ExchangePlatform.match();
+
+        try{
+            assertTrue(Trader.owns("Manwel", "building", 5));
+        }
+        catch(NonExistingException e){
+            fail();
+        }
+
+        try{
+            assertTrue(Trader.owns("Christian", "building", 5));
+        }
+        catch(NonExistingException e){
+            fail();
+        }
+    }
+}
