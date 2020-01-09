1550421df6f1e83b5a3352fd92e87e2527a02e8f
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2020-01-09 21:34:38 +0100

    added tests for buyOrder.book()

--- 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task2/test/BuyOrderTest.java
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task2/test/BuyOrderTest.java
@@ -3,8 +3,23 @@ import static org.junit.Assert.*;
 
 public class BuyOrderTest {
     @Test
-    public void buyOrderTesr1(){
+    public void buyOrderTest1(){
 
+        String trader = Generate.randomString(10);
+        double price = Generate.randomDouble(1000000, 0);
+        int quantity = (int) Generate.randomDouble(1000000, 0);
+        Trader.register(trader, price * quantity);
+
+
+        //buy order with non-existing user
+        boolean expected = false;
+        boolean actual = BuyOrder.book(Generate.randomString(9), Generate.randomString(10), price, quantity);
+        assertEquals(expected, actual);
+
+        //successful buy order
+        expected = true;
+        actual = BuyOrder.book(trader, Generate.randomString(10), price, quantity);
+        assertEquals(expected, actual);
     }
 
 }
