e8836801d5fe6e0c92ad5137864219c5c62f779c
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2020-01-11 20:27:33 +0100

    fixed scripts

--- 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task2/src/compile.sh
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task2/src/compile.sh
@@ -1,1 +1,3 @@
 javac -d ../out/production main/*.java
+
+javac -d ../out/test/task2 -cp .:../junit-4.13.jar:../hamcrest-core-2.2.jar test/*.java
---
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task2/src/main/compile.sh
@@ -0,0 +1,2 @@
+javac -d ../../out/production *.java
+javac -d ../../out/test/task2 -cp .:../../junit-4.13.jar:../../hamcrest-core-2.2.jar *.java
