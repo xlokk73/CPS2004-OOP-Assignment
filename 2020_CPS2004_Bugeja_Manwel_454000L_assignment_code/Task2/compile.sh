dae531ab62d0371c34e33996d0664d2ac48420f7
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2020-01-13 20:11:39 +0100

    fixed scripts

---
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task2/compile.sh
@@ -0,0 +1,2 @@
+javac -d out/production/task2 src/main/*.java
+javac -d out/test/task2 -classpath src/main:lib/* src/test/*.java
---
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task2/torun.sh
@@ -0,0 +1,7 @@
+java -cp lib/*:out/test/task2 org.junit.runner.JUnitCore TraderTest
+java -cp lib/*:out/test/task2 org.junit.runner.JUnitCore ListerTest
+java -cp lib/*:out/test/task2 org.junit.runner.JUnitCore SecurityTest
+java -cp lib/*:out/test/task2 org.junit.runner.JUnitCore BuyOrderTest
+java -cp lib/*:out/test/task2 org.junit.runner.JUnitCore SellOrderTest
+java -cp lib/*:out/test/task2 org.junit.runner.JUnitCore ExchangePlatformTest
+java -cp out/production/task2/. Main
