09f0207566ded7264ffd41996958b84f2aeaf708
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2020-01-13 10:22:35 +0100

    fixed scripts

--- 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task2/src/test/torun.sh
+++ 2020_CPS2004_Bugeja_Manwel_454000L_assignment_code/Task2/src/test/torun.sh
@@ -1,1 +1,7 @@
 java -cp ../../lib/junit-4.12.jar:../../lib/hamcrest-core-1.3.jar:../../out/test/task2/. org.junit.runner.JUnitCore TraderTest
+java -cp ../../lib/junit-4.12.jar:../../lib/hamcrest-core-1.3.jar:../../out/test/task2/. org.junit.runner.JUnitCore ListerTest
+java -cp ../../lib/junit-4.12.jar:../../lib/hamcrest-core-1.3.jar:../../out/test/task2/. org.junit.runner.JUnitCore SecurityTest
+java -cp ../../lib/junit-4.12.jar:../../lib/hamcrest-core-1.3.jar:../../out/test/task2/. org.junit.runner.JUnitCore BuyOrderTest
+java -cp ../../lib/junit-4.12.jar:../../lib/hamcrest-core-1.3.jar:../../out/test/task2/. org.junit.runner.JUnitCore SellOrderTest
+java -cp ../../lib/junit-4.12.jar:../../lib/hamcrest-core-1.3.jar:../../out/test/task2/. org.junit.runner.JUnitCore ExchangePlatformTest
+java -cp ../../out/production/task2/. Main
